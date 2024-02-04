76b0a982c6d2c442841183d1b8aa3f8fdea29b33
Scuti1 <uscuti95@gmail.com>
2024-02-05 00:23:43 +0800

    add page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import { Button, Input } from '@nextui-org/react'
+import React from 'react'
+import Link from 'next/link'
+
 export default function Home() {
-  return <></>
+  return (
+    <>
+      <div className="max-w-sm mx-auto">
+        <div className="mb-5">
+          <Input className="flex-initial" type="text" label="Тоглоомын нэр" />
+        </div>
+        <div className="mb-5">
+          <Input className="flex-initial" type="text" label="Нэр" />
+        </div>
+        <Link href="/game">
+          <Button color="warning" size="lg" className="w-full">
+            Тоглоом эхлүүлэх
+          </Button>
+        </Link>
+      </div>
+    </>
+  )
 }
